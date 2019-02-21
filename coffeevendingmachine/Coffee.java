1b471cd9f64b67b42040d41f3273cfecdfb4c63c
LuiGee3471 <yjs3475@gmail.com>
2019-02-21 09:57:52 +0900

    Revert "no message"
    This reverts commit 882fe146f1b5e9ed9a02d0927674afc15cae46a2.

--- coffeevendingmachine/Coffee.java
+++ coffeevendingmachine/Coffee.java
@@ -1,5 +1,7 @@
 package coffeevendingmachine;
 
+import java.util.Arrays;
+
 public class Coffee {
 	private String coffeeName; // 커피 이름
 	private int coffeeId;      // 커피 고유번호
@@ -12,6 +14,12 @@ public class Coffee {
 
 	public void setCoffeeName(String coffeeName) {
 		this.coffeeName = coffeeName;
+	}
+
+	@Override
+	public String toString() {
+		return "Coffee [coffeeName=" + coffeeName + ", coffeeId=" + coffeeId + ", recipe=" + Arrays.toString(recipe)
+				+ ", coffeePrice=" + coffeePrice + "]";
 	}
 
 	public int getCoffeeId() {
--- coffeevendingmachine/VM.java
+++ coffeevendingmachine/VM.java
@@ -5,6 +5,13 @@ public class VM {
 	public static void main(String[] args) {
 		//Coffee c = new Coffee("아메리카노", 1, new int[] { 1,1,1,1 }, 1000);
 		Machine m = new Machine();
-		m.Coffee();
+		while (true) {
+			Coffee c = m.displayMenu();
+			if (c == null)
+				continue;
+			c = m.setUserRecipe(c);
+			m.displaySum(c);
+			m.refund(c);
+		}
 	}
 }
