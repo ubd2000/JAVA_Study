9efcf9939401f4e5e3c8b0eded681e9422ad0621
LuiGee3471 <yjs3475@gmail.com>
2019-02-21 09:58:30 +0900

    Revert "관리자 모드 종료 후 문제 수정"
    This reverts commit 4f13a2518c194db08d62d09973278ca5952da553.

--- coffeevendingmachine/VM.java
+++ coffeevendingmachine/VM.java
@@ -6,12 +6,7 @@ public class VM {
 		//Coffee c = new Coffee("아메리카노", 1, new int[] { 1,1,1,1 }, 1000);
 		Machine m = new Machine();
 		while (true) {
-			Coffee c = m.displayMenu();
-			if (c == null)
-				continue;
-			c = m.setUserRecipe(c);
-			m.displaySum(c);
-			m.refund(c);
+		    m.Coffee();
 		}
 	}
 }
